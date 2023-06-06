336e51c4613058b3f92521949fd1ad505cb23d2c
UIT-20520724 <20520724@gm.uit.edu.vn>
2023-06-06 19:23:08 +0700

    Add project files.

---
+++ Quản ly đăng ký môn học và thanh toán học phí/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Quản_ly_đăng_ký_môn_học_và_thanh_toán_học_phí
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
